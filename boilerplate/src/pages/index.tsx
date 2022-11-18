4cb57ff439d5fe8ab8ca6043f8425f93b0726900
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-11-18 10:48:49 -0300

    Add husky and lint-staged

---
+++ boilerplate/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Boilerplate</title>
+      </Head>
+
+      <h1>Hello, World</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At vero fugiat
+        voluptatum animi earum quod, fuga dolore, et iste optio voluptatibus
+        quis? Magni deserunt praesentium sequi ab labore, officiis voluptatem.
+      </p>
+    </div>
+  );
+}
