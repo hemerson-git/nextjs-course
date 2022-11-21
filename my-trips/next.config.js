fcc0ea64aa3249c8ae681f986aebbc98613f5e07
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-11-21 02:46:06 -0300

    CREATE my-trips, and add leaflet

---
+++ my-trips/next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+withPWA;
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd,
+    sw: 'service-worker.js',
+  },
+});
+
+module.exports = nextConfig;
