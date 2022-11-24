62fe199be54b88cd5ebe239ad6b766e663fd1522
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-11-24 10:20:04 -0300

    ADD Mapbox to the Map Components

--- my-trips/src/components/Map/index.tsx
+++ my-trips/src/components/Map/index.tsx
@@ -15,21 +15,38 @@ export interface MapProps {
   places?: Place[];
 }
 
+const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY;
+const MAPBOX_API_USER_ID = process.env.NEXT_PUBLIC_MAPBOX_API_USER_ID;
+const MAPBOX_API_STYLE_ID = process.env.NEXT_PUBLIC_MAPBOX_API_STYLE_ID;
+
+function CustomTileLayer() {
+  return MAPBOX_API_KEY ? (
+    <TileLayer
+      attribution='&copy; <a href="https://apps.mapbox.com/feedback">Mapbox </a> ©
+        <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      url={`https://api.mapbox.com/styles/v1/${MAPBOX_API_USER_ID}/${MAPBOX_API_STYLE_ID}/tiles/256/{z}/{x}/{y}?access_token=${MAPBOX_API_KEY}`}
+    />
+  ) : (
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  );
+}
+
 export function Map({ places }: MapProps) {
   const router = useRouter();
 
   return (
     <MapContainer
       center={[-14.8931516, -40.8447112]}
-      zoom={4.2}
+      zoom={3.4}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <CustomTileLayer />
       {places?.map((place) => (
         <Marker
+          key={place.id}
           position={[place.location.latitude, place.location.longitude]}
           eventHandlers={{
             click: () => {
@@ -37,7 +54,7 @@ export function Map({ places }: MapProps) {
             },
           }}
         >
-          <Popup>{place.name}</Popup>
+          {/* <Popup>{place.name}</Popup> */}
         </Marker>
       ))}
     </MapContainer>
