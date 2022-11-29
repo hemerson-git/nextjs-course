2c99c0284a2fe51264f7b62193d4480bad4df929
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-11-28 22:35:09 -0300

    FIX: blank bounds map, maxZoomOut, CHAGES: Zoom Control Position

--- my-trips/src/components/Map/index.tsx
+++ my-trips/src/components/Map/index.tsx
@@ -1,5 +1,7 @@
+import { latLng, latLngBounds } from 'leaflet';
 import { useRouter } from 'next/router';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Plus } from 'phosphor-react';
+import { MapContainer, TileLayer, Marker, ZoomControl } from 'react-leaflet';
 
 interface Place {
   id: string;
@@ -25,25 +27,42 @@ function CustomTileLayer() {
       attribution='&copy; <a href="https://apps.mapbox.com/feedback">Mapbox </a> ©
         <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       url={`https://api.mapbox.com/styles/v1/${MAPBOX_API_USER_ID}/${MAPBOX_API_STYLE_ID}/tiles/256/{z}/{x}/{y}?access_token=${MAPBOX_API_KEY}`}
+      noWrap
+      minZoom={3}
     />
   ) : (
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      noWrap
+      minZoom={3}
     />
   );
 }
 
+function CustomZoomControls() {
+  return <ZoomControl zoomOutText="-" zoomInText="+" position="bottomleft" />;
+}
+
 export function Map({ places }: MapProps) {
   const router = useRouter();
+
+  const corner1 = latLng(-90, -180);
+  const corner2 = latLng(90, 180);
+  const bounds = latLngBounds(corner1, corner2);
 
   return (
     <MapContainer
       center={[-14.8931516, -40.8447112]}
       zoom={3.4}
       style={{ height: '100%', width: '100%' }}
+      bounceAtZoomLimits
+      maxBounds={bounds}
+      maxBoundsViscosity={1}
+      zoomControl={false}
     >
       <CustomTileLayer />
+      <CustomZoomControls />
       {places?.map((place) => (
         <Marker
           key={place.id}
